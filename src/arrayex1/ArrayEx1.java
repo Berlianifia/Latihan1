8c69365ef5fcb1d967d940fd58c2c95268150ccc
Berliani Fia Maharis Arikhala <72061864+Berlianifia@users.noreply.github.com>
2021-03-25 16:29:10 +0700

    Initial Commit

---
+++ src/arrayex1/ArrayEx1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayex1;
+
+/**
+ *
+ * @author Berliani
+ */
+public class ArrayEx1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //membuat variabel
+        String nama, alamat;
+        int tinggi;
+        
+        //mengisi variabel
+        nama = "berliani";
+        alamat = "kediri";
+        tinggi = 155;
+      
+        
+        System.out.println("nama: " +nama);
+        System.out.println("alamat: " +alamat);
+        System.out.println("tinggi: " + tinggi + " cm");
+        
+    } 
+}
